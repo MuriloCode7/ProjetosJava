a3b63332d69059ae4bd34b06928f7e0e158ad21e
MuriloMagal <murilomagalhaes60@gmail.com>
2023-08-31 14:13:53 -0300

    Remoção do modo iniciante

--- CampoMinadoV1.0/src/JogoCampoMinado/MainCampoMinado.java
+++ CampoMinadoV1.0/src/JogoCampoMinado/MainCampoMinado.java
@@ -10,33 +10,23 @@ public class MainCampoMinado {
             ImageIcon iconJogo = new ImageIcon(getClass().getResource("/midia/iconJogo.png"));
 
 
-		//Declaração da variável para a escolha do nível do jogo.
-		String[] niveis = { "Nível Iniciante: 9x9", "Nível Intermediário: 16x16"};
-
 		//Escolha do nível do jogo.
-		String nivel = (String) JOptionPane.showInputDialog(null, "Selecione a dificuldade do jogo: ", "Campo Minado", 3,
-				iconJogo, niveis, null);
+		int comecar = JOptionPane.showConfirmDialog(null, "Deseja iniciar o jogo?", "Campo Minado",  
+                        JOptionPane.YES_NO_OPTION, 0, iconJogo);
+                
+                if (comecar == 0) {
+                    //Instanciação da janela do jogo.
+                    Tabuleiro tab = new Tabuleiro();
 
-		//Instanciação da janela do jogo.
-		Tabuleiro tab = new Tabuleiro();
+                    tab.setTam(16);
+                    tab.setMinas(40);
+                    tab.setBounds(350,100,900, 672);
+
+
+                    //Chamada do método que gera as células no tabuleiro.
+                    tab.geraCelulas();
+                }
 		
-		if (nivel == niveis[0]) {
-			//Se o nível escolhido for o "iniciante", o tabuleiro será setado com o tamanho de 9x9 
-			//e com 10 minas espalhadas pelo tabuleiro (O método setSize define as dimensões do JFrame).
-			tab.setTam(9);
-			tab.setMinas(10);
-			tab.setBounds(450,150,700, 486);
-		} else {
-			//Se o nível escolhido for o "intermediário", o tabuleiro será setado com o tamanho de 16x16 
-			//e com 40 minas espalhadas pelo tabuleiro (O método setSize define as dimensões do JFrame).
-			tab.setTam(16);
-			tab.setMinas(40);
-			tab.setBounds(350,100,900, 672);
-		} 
-
-		//Chamada do método que gera as células no tabuleiro.
-		tab.geraCelulas();
-                tab = null;
 
 	}
 
